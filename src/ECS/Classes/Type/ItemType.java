0a436ce871bf8364bec7b2ea376b894dd256c77b
이수헌 <wizard872@naver.com>
2020-04-07 00:44:33 +0900

    .

--- src/ECS/Classes/Type/ItemType.java
+++ src/ECS/Classes/Type/ItemType.java
@@ -6,6 +6,7 @@ public class ItemType {
     public static final int HP_POTION = 1;
     public static final int MP_POTION = 2;
     public static final int SPEED_POTION = 3;
+    
     public static final int DEFENSE_POTION = 4;
     public static final int ATTACK_POTION = 5;
 
