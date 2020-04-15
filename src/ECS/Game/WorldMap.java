04989332ad5ebefb0a8d6b7a31aaa3f1ba9c9f71
Ellio <tpdl0825@gmail.com>
2020-04-15 22:02:54 +0900

    .

--- src/ECS/Game/WorldMap.java
+++ src/ECS/Game/WorldMap.java
@@ -3678,7 +3678,7 @@ public class WorldMap {
         entity.characterComponent.characterName = playerData.characterName;
         entity.characterComponent.characterType = characterData.guardianType;
         entity.attribute = characterData.elemental;
-        entity.attribute = ElementalType.RED;
+        entity.attribute++;
 
         System.out.println("캐릭터 타입 : " + entity.characterComponent.characterType);
 
