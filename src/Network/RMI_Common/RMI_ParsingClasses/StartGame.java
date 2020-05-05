7c85c5b9688ad463be6ccf32513a04e03faaff94
이수헌 <wizard872@naver.com>
2020-05-05 22:47:19 +0900

    캐릭터, 몬스터, 타워, 방호벽, 크리스탈에 boolean isShieldActivated 추가

--- src/Network/RMI_Common/RMI_ParsingClasses/StartGame.java
+++
@@ -1,9 +0,0 @@
-package Network.RMI_Common.RMI_ParsingClasses;
-
-import Network.RMI_Common.RMI_ParsingClasses.*;
-import Network.AutoCreatedClass.*;
-import java.util.LinkedList;
-
-public class StartGame {
-
-}
